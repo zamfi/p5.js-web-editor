63f938a357812adf19e20081633e192699db4f7e
J.D. Zamfirescu <zamfi@alum.mit.edu>
2020-08-02 13:19:15 -0700

    ethereal!

--- server/utils/mail.js
+++ server/utils/mail.js
@@ -3,16 +3,16 @@
  */
 
 import nodemailer from 'nodemailer';
-import mg from 'nodemailer-mailgun-transport';
+// import mg from 'nodemailer-mailgun-transport';
 
-const auth = {
-  api_key: process.env.MAILGUN_KEY,
-  domain: process.env.MAILGUN_DOMAIN,
-};
+// const auth = {
+//   api_key: process.env.MAILGUN_KEY,
+//   domain: process.env.MAILGUN_DOMAIN,
+// };
 
 class Mail {
   constructor() {
-    this.client = nodemailer.createTransport(mg({ auth }));
+    this.createAccountAndTransport();
     this.sendOptions = {
       from: process.env.EMAIL_SENDER,
     };
@@ -42,6 +42,20 @@ class Mail {
 
   send(data, callback) {
     return this.dispatchMail(data, callback);
+  }
+
+  async createAccountAndTransport() {
+    this.testAccount = await nodemailer.createTestAccount();
+    console.log('created fake ethereal account at', JSON.stringify(this.testAccount));
+    this.client = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: this.testAccount.user, // generated ethereal user
+        pass: this.testAccount.pass, // generated ethereal password
+      },
+    });
   }
 }
 
